78fa262f5b1141d777cc2d902c258a4d78ef48bb
Lina Jesus <linajesus99@gmail.com>
2020-06-17 01:11:56 +0100

    Interfaces with PrimeFaces

--- TP2/build/generated-sources/ap-source-output/entities/Reservar_.java
+++ TP2/build/generated-sources/ap-source-output/entities/Reservar_.java
@@ -7,7 +7,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-06-10T22:21:27")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-06-13T22:26:31")
 @StaticMetamodel(Reservar.class)
 public class Reservar_ { 
 
